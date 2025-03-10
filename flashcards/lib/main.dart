e655e3dd813aa27c89bc0dfca49c126bf448f532
Ali Raza <alirazakhalid1129@gmail.com>
2025-03-10 17:29:04 +0500

    add basic routes

--- flashcards/lib/main.dart
+++ flashcards/lib/main.dart
@@ -1,122 +1,147 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "FlashCards App",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.grey[100],
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: DeckSelectionScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+class Flashcard {
+  final String question;
+  final String answer;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  const Flashcard({required this.question, required this.answer});
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class Deck {
+  final String title;
+  final List<Flashcard> cards;
 
-  void _incrementCounter() {
+  Deck({ required this.title, required this.cards });
+}
+
+class DeckSelectionScreen extends StatelessWidget {
+  final List<Deck> decks = [
+    Deck(
+        title: "Flutter Basics",
+        cards: [
+          Flashcard(question: "What is Flutter?", answer: "Google UI toolkit for building natively compiled apps"),
+          Flashcard(question: "What language does flutter use?", answer: "Dart"),
+        ]
+    ),
+    Deck(
+        title: "Dart",
+        cards: [
+          Flashcard(question: "what is dart mixins?", answer: "way to reuse code across multiple class."),
+          Flashcard(question: "what is null safety in dart?", answer: "Feature that prevents null reference exception."),
+        ]
+    )
+  ];
+
+  @override Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Choose a deck")),
+      body: ListView.builder(
+          itemCount: decks.length,
+          itemBuilder: (context, index) {
+            return Card(
+              margin: const EdgeInsets.all(8),
+              child: ListTile(
+                title: Text(decks[index].title),
+                subtitle: Text('${decks[index].cards.length} cards'),
+                trailing: Icon(Icons.arrow_forward),
+                onTap: ()  {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => FlashcardScreen(deck: decks[index])
+                      )
+                  );
+                },
+              ),
+            );
+          }),
+    );
+  }
+}
+
+class FlashcardScreen extends StatefulWidget {
+  final Deck deck;
+  const FlashcardScreen({super.key, required this.deck});
+
+  @override
+  _FlashcardScreenState createState() => _FlashcardScreenState();
+}
+
+class _FlashcardScreenState extends State<FlashcardScreen> {
+  bool _showQuestion = true;
+  int _currentCardIndex = 0;
+
+  void _toggleCard() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _showQuestion = !_showQuestion;
+    });
+  }
+
+  void _nextCard() {
+    setState(() {
+      _currentCardIndex = (_currentCardIndex+1) % widget.deck.cards.length;
+      _showQuestion = true;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(widget.deck.title),
+        leading: IconButton(
+            onPressed: () => Navigator.pop(context),
+            icon: Icon(Icons.arrow_back)),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            GestureDetector(
+              onTap: _toggleCard,
+              child: Card(
+                elevation: 8,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8),),
+                child: Container(
+                  width: 300,
+                  height: 200,
+                  padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                  child: Center(
+                    child: Text(
+                      _showQuestion ? widget.deck.cards[_currentCardIndex].question:
+                      widget.deck.cards[_currentCardIndex].answer,
+                      style: const TextStyle(fontSize: 20),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ),
             ),
+            const SizedBox(height: 30),
+            ElevatedButton(onPressed: _nextCard, child: const Text("next ")),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
