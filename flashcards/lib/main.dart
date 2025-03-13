de0d150eca7de44de9d871fd9cf53688a0ba82ad
Ali Raza <alirazakhalid1129@gmail.com>
2025-03-14 04:56:36 +0500

    add toggleTheme btn

--- flashcards/lib/main.dart
+++ flashcards/lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flashcards/providers/theme_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'models/flashcard_provider.dart';
 import 'screens/add_card_screen.dart';
 import 'screens/deck_screen.dart';
 import 'screens/home_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   await Hive.initFlutter();
@@ -25,17 +25,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "FlashCards App",
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: "Lato",
-        textTheme: const TextTheme(
-          headlineLarge: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-          bodyMedium: TextStyle(fontSize: 16),
-        ),
-      ),
+      theme: themeProvider.getTheme(),
+      // theme: ThemeData(
+      //   primarySwatch: Colors.deepPurple,
+      //   fontFamily: "Lato",
+      //   textTheme: const TextTheme(
+      //     headlineLarge: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+      //     bodyMedium: TextStyle(fontSize: 16),
+      //   ),
+      // ),
         initialRoute: '/',
         routes: {
           '/': (context) => const HomeScreen(),
--- flashcards/lib/providers/theme_provider.dart
+++ flashcards/lib/providers/theme_provider.dart
@@ -1,14 +1,40 @@
-
 import 'package:flutter/material.dart';
 
-class ThemeProvider with ChangeNotifier{
+class ThemeProvider with ChangeNotifier {
   bool _isDarkMode = false;
+
   bool get isDarkMode => _isDarkMode;
-  
-  void toggleTheme(){
+
+  void toggleTheme() {
     _isDarkMode = !_isDarkMode;
     notifyListeners();
   }
 
-  ThemeData getTheme() => _isDarkMode ? ThemeData.dark() : ThemeData.light();
+  ThemeData getTheme() {
+    return _isDarkMode ? _darkTheme : _lightTheme;
+  }
+
+  ThemeData get _lightTheme {
+    return ThemeData(
+      brightness: Brightness.light,
+      fontFamily: 'Lato',
+      primarySwatch: Colors.blue,
+      textTheme: const TextTheme(
+        headlineLarge: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        bodyMedium: TextStyle(fontSize: 16),
+      ),
+    );
+  }
+
+  ThemeData get _darkTheme {
+    return ThemeData(
+      brightness: Brightness.dark,
+      fontFamily: 'Lato',
+      primarySwatch: Colors.blue,
+      textTheme: const TextTheme(
+        headlineLarge: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white),
+        bodyMedium: TextStyle(fontSize: 16, color: Colors.white),
+      ),
+    );
+  }
 }
