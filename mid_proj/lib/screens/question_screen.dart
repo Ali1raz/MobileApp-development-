d30870519c7591eb95e26570f200f0e43449011d
Ali Raza <alirazakhalid1129@gmail.com>
2025-04-21 00:00:30 +0500

    question screen layout

--- mid_proj/lib/screens/question_screen.dart
+++ mid_proj/lib/screens/question_screen.dart
@@ -12,7 +12,57 @@ class _QuestionScreenState extends State<QuestionScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Question")),
-      body: Center(child: Text("Questions coming soon")),
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: 18),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "0 / 10",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.white60,
+                  ),
+                ), // current / total
+              ],
+            ),
+            SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "0",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                    color: Colors.green,
+                  ),
+                ), // correct
+                SizedBox(width: 40),
+                Text(
+                  "10",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                    color: Colors.red,
+                  ),
+                ), // wrong
+              ],
+            ),
+            const Spacer(),
+            Text("3 X 6 = 18?", style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),), // question
+            SizedBox(height: 38,),
+            Padding(padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20), child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, crossAxisAlignment: CrossAxisAlignment.center, children: [
+              CircleAvatar(backgroundColor: Colors.red, radius: 40, child: const Icon(Icons.close, size: 40,),),
+              CircleAvatar(backgroundColor: Colors.green, radius: 40, child: const Icon(Icons.check, size: 40,),),
+            ],),),
+            SizedBox(height: 28,)
+          ],
+        ),
+      ),
     );
   }
 }
