dbb7ad277f78f12fa6c43465f48afb34e94baea0
Ali Raza <alirazakhalid1129@gmail.com>
2025-05-01 06:22:10 +0500

    add fetch api with 404 status check

--- github_api/lib/main.dart
+++ github_api/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -31,6 +32,34 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _controller = TextEditingController();
+  bool? success;
+
+  Future<void> fetchData(String username) async {
+    final url = Uri.parse("https://api.github.com/users/$username/events");
+    try {
+      final response = await http.get(url);
+
+      if (response.statusCode == 404) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text("404: User not found")));
+        setState(() {
+          success = false;
+        });
+        return;
+      }
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Success")));
+      setState(() {
+        success = true;
+      });
+    } catch (error) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Error: $error")));
+    }
+  }
 
   @override
   void dispose() {
@@ -73,13 +102,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       child: ElevatedButton(
                         onPressed: () {
                           if (_formKey.currentState!.validate()) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text(
-                                  "Entered username: ${_controller.text}",
-                                ),
-                              ),
-                            );
+                            fetchData(_controller.text);
                           }
                         },
                         child: const Text('Search'),
@@ -88,6 +111,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   ],
                 ),
               ),
+
+              SizedBox(height: 40),
             ],
           ),
         ),
