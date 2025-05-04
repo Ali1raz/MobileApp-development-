5fb51a5fb8e38449a75f112486fbf0436d230019
Ali Raza <alirazakhalid1129@gmail.com>
2025-05-04 14:04:14 +0500

    add resultScreen

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -1,22 +1,13 @@
 import 'package:flutter/material.dart';
 import 'constants/theme.dart';
+import 'screens/result_screen.dart';
 
 void main() => runApp(const BMICalculator());
 
 enum Gender { male, female }
 
-class BMICalculator extends StatefulWidget {
+class BMICalculator extends StatelessWidget {
   const BMICalculator({super.key});
-
-  @override
-  State<BMICalculator> createState() => _BMICalculatorState();
-}
-
-class _BMICalculatorState extends State<BMICalculator> {
-  Gender? selectedGender;
-  double height = 120.0;
-  int weight = 60;
-  int age = 20;
 
   @override
   Widget build(BuildContext context) {
@@ -26,105 +17,163 @@ class _BMICalculatorState extends State<BMICalculator> {
         scaffoldBackgroundColor: AppTheme.scaffoldBackgroundColor,
       ),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: Text("BMI Calculator")),
-        body: SafeArea(
-          child: Column(
-            children: [
-              Expanded(
-                child: Row(
-                  children: [
-                    GenderCard(
-                      gender: Gender.male,
-                      isSelected: selectedGender == Gender.male,
-                      onTap: () {
-                        setState(() {
-                          selectedGender = Gender.male;
-                        });
-                      },
-                    ),
-                    GenderCard(
-                      gender: Gender.female,
-                      isSelected: selectedGender == Gender.female,
-                      onTap: () {
-                        setState(() {
-                          selectedGender = Gender.female;
-                        });
-                      },
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: CardSection(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("HEIGHT", style: TextStyle(color: Colors.white)),
-                      Text(
-                        "${height.round()} cm",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Slider(
-                        value: height,
-                        min: 120.0,
-                        max: 220.0,
-                        divisions: 100,
-                        label: '${height.round()} cm',
-                        onChanged: (val) {
-                          setState(() {
-                            height = val;
-                          });
-                        },
-                        activeColor: AppTheme.activeColor,
-                        inactiveColor: AppTheme.inactiveColor,
-                      ),
-                    ],
+      home: InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  Gender? selectedGender;
+  double height = 120.0;
+  int weight = 60;
+  int age = 20;
+
+  double calculateBMI() {
+    return weight / ((height / 100) * (height / 100));
+  }
+
+  String getResult(double bmi) {
+    if (bmi >= 25) {
+      return 'Overweight';
+    } else if (bmi > 18.5) {
+      return 'Normal';
+    } else {
+      return 'Underweight';
+    }
+  }
+
+  String getInterpretation(double bmi) {
+    if (bmi >= 25) {
+      return 'You have a higher than normal body weight. Try to exercise more.';
+    } else if (bmi > 18.5) {
+      return 'You have a normal body weight. Good job!';
+    } else {
+      return 'You have a lower than normal body weight. You can eat a bit more.';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("BMI Calculator")),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  GenderCard(
+                    gender: Gender.male,
+                    isSelected: selectedGender == Gender.male,
+                    onTap: () {
+                      setState(() {
+                        selectedGender = Gender.male;
+                      });
+                    },
                   ),
-                ),
+                  GenderCard(
+                    gender: Gender.female,
+                    isSelected: selectedGender == Gender.female,
+                    onTap: () {
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                    },
+                  ),
+                ],
               ),
-              Expanded(
-                child: Row(
+            ),
+            Expanded(
+              child: CardSection(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    ValueCard(
-                      label: "WEIGHT",
-                      value: weight,
-                      onIncrement: () {
-                        setState(() {
-                          weight++;
-                        });
-                      },
-                      onDecrement: () {
-                        setState(() {
-                          weight--;
-                        });
-                      },
+                    Text("HEIGHT", style: TextStyle(color: Colors.white)),
+                    Text(
+                      "${height.round()} cm",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                    ValueCard(
-                      label: "AGE",
-                      value: age,
-                      onIncrement: () {
+                    Slider(
+                      value: height,
+                      min: 120.0,
+                      max: 220.0,
+                      divisions: 100,
+                      label: '${height.round()} cm',
+                      onChanged: (val) {
                         setState(() {
-                          age++;
+                          height = val;
                         });
                       },
-                      onDecrement: () {
-                        setState(() {
-                          age--;
-                        });
-                      },
+                      activeColor: AppTheme.activeColor,
+                      inactiveColor: AppTheme.inactiveColor,
                     ),
                   ],
                 ),
               ),
-              Container(
-                color: AppTheme.buttonColor,
-                height: 60,
-                width: double.infinity,
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  ValueCard(
+                    label: "WEIGHT",
+                    value: weight,
+                    onIncrement: () {
+                      setState(() {
+                        weight++;
+                      });
+                    },
+                    onDecrement: () {
+                      setState(() {
+                        weight--;
+                      });
+                    },
+                  ),
+                  ValueCard(
+                    label: "AGE",
+                    value: age,
+                    onIncrement: () {
+                      setState(() {
+                        age++;
+                      });
+                    },
+                    onDecrement: () {
+                      setState(() {
+                        age--;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: AppTheme.buttonColor,
+              height: 60,
+              width: double.infinity,
+              child: GestureDetector(
+                onTap: () {
+                  final bmi = calculateBMI();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ResultScreen(
+                        bmi: bmi,
+                        result: getResult(bmi),
+                        interpretation: getInterpretation(bmi),
+                      ),
+                    ),
+                  );
+                },
                 child: Center(
                   child: Text(
                     "CALCULATE",
@@ -132,8 +181,8 @@ class _BMICalculatorState extends State<BMICalculator> {
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
---
+++ bmi_calculator/lib/screens/result_screen.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import '../constants/theme.dart';
+
+class ResultScreen extends StatelessWidget {
+  final double bmi;
+  final String result;
+  final String interpretation;
+
+  const ResultScreen({
+    super.key,
+    required this.bmi,
+    required this.result,
+    required this.interpretation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('BMI Result')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(15),
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                'Your Result',
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Container(
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: AppTheme.cardColor,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    result.toUpperCase(),
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    bmi.toStringAsFixed(1),
+                    style: TextStyle(
+                      fontSize: 100,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    interpretation,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 22,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Container(
+              color: AppTheme.buttonColor,
+              margin: EdgeInsets.only(top: 10),
+              width: double.infinity,
+              height: 80,
+              child: Center(
+                child: Text(
+                  'RE-CALCULATE',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+} 
