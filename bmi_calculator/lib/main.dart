dde53a43f4c5eb3e45ddc1e1cbf7a645e213d291
Ali Raza <alirazakhalid1129@gmail.com>
2025-05-04 11:27:09 +0500

    add dark theme

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -8,9 +8,12 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
       home: Scaffold(
         appBar: AppBar(title: Text("BMI Calculator"),),
-        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
             children: [
