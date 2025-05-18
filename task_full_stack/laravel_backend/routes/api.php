3fa49bd948aaaf6e8464d55fc583b3cbe48c96f8
Ali Raza <alirazakhalid1129@gmail.com>
2025-05-18 14:26:14 +0500

    add StudentController with api/student/login

--- task_full_stack/laravel_backend/routes/api.php
+++ task_full_stack/laravel_backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Student\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,10 +17,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/admin/login', [AuthenticatedSessionController::class, 'store']);
+
 Route::middleware(['auth:sanctum', 'role:admin'])->prefix('admin')->group(function () {
     // all routes for admin only
     Route::get('/dashboard', [AdminController::class, 'dashboard']);
     Route::post('register-student', [AdminController::class, 'registerStudent']);
 });
 
-Route::post('/admin/login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/student/login', [StudentController::class, 'store']);
+
+Route::middleware(['auth:sanctum', 'role:student'])->prefix('student')->group(function () {
+    // all routes for admin only
+    Route::get('/dashboard', [StudentController::class, 'dashboard']);
+});
